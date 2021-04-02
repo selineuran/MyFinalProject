c6e124735a2ebda41812b1285da488103ec0638d
selineuran <suran1@sheffield.ac.uk>
2021-04-02 14:20:06 +0100

    Reorganising graphs in .Rmd and adding to ToDo list in .R file

--- project.R
+++ project.R
@@ -1,4 +1,4 @@
-#Working project-----------------------------
+#Developing code for the project-----------------------------
 
 library(ggplot2)
 library(readxl)
@@ -8,26 +8,8 @@ library(dplyr)
 library(tidyr)
 library(tidyverse)
 
-rawdata <- read_excel("~/myfinalproject/processed/rs_statistics.xlsx")
-
-rs1<- rawdata %>% slice(1:2)
-
-rs1$`Local authority ONS code`<-NULL
-rs1$`Region ONS code`<-NULL
-rs1$`Local authority`<-NULL
-
-rs1_long <- rs1 %>% 
-  gather(Year, Number, -Region)
-rs1_long
-
-rs1plot <- ggplot(rs1_long, aes(x = Year, y = Number, color = Region, group = Region)) +
-  geom_point() +
-  geom_line() +
-  scale_color_brewer(palette = 'Dark2') +
-  theme_classic(base_size = 12)
-
-rs1plot
-ggsave("figures/rs1plot.png")
+#------------------------------------------------------------------------------
+#The first main showcase visualisation 
 
 rawdata <- read_excel("~/myfinalproject/processed/rs_statistics.xlsx")
 View(rawdata)
@@ -54,6 +36,34 @@ rs2plot
 
 ggsave("figures/rs2plot.png")
 
+#------------------------------------------------------------------------------
+#The secondary supporting graph 
+
+rawdata <- read_excel("~/myfinalproject/processed/rs_statistics.xlsx")
+View(rawdata)
+
+rs1<- rawdata %>% slice(1:2)
+
+rs1$`Local authority ONS code`<-NULL
+rs1$`Region ONS code`<-NULL
+rs1$`Local authority`<-NULL
+
+rs1_long <- rs1 %>% 
+  gather(Year, Number, -Region)
+rs1_long
+
+rs1plot <- ggplot(rs1_long, aes(x = Year, y = Number, color = Region, group = Region)) +
+  geom_point() +
+  geom_line() +
+  scale_color_brewer(palette = 'Dark2') +
+  theme_classic(base_size = 12)
+
+rs1plot
+ggsave("figures/rs1plot.png")
+
+
+#------------------------------------------------------------------------------
+#The third supporting graph - this time a piechart
 
 data4 <- read_excel("~/myfinalproject/processed/data4.xlsx")
 View(data4)
@@ -61,15 +71,16 @@ View(data4)
 d4 <- data.frame(
   Group = c("Leeds", "Sheffield", "Kingston upon Hull, City of", "Doncaster", 
             "East Riding of Yorkshire", "Scarborough", "Barnsley"),
-  value = c(35, 24, 19, 13, 11, 11, 10)
-)
+  value = c(35, 24, 19, 13, 11, 11, 10))
 
-# Barplot
+# First you must generate a barplot of the data
 bp<- ggplot(d4, aes(x="", y=value, fill=Group))+
   geom_bar(width = 1, stat = "identity")
 
+#Then plot the data into a piechart as below
 pie <- bp + coord_polar("y", start=0)
 
+#Use a template to stylise the piechart to your individual preference 
 blank_theme <- theme_minimal()+
   theme(
     axis.title.x = element_blank(),
@@ -79,31 +90,38 @@ blank_theme <- theme_minimal()+
     plot.title=element_text(size=14, face="bold")
   )
 
-#The piechart....
+#This template is then added to the piechart and the aesthetics can be adjusted 
+  #as needed The piechart....
 pie<- pie + scale_fill_brewer(palette="Dark2") + blank_theme +
   theme(axis.text.x=element_blank())+
   geom_text(aes(y = value/3 + c(0, cumsum(value)[-length(value)]), 
                 label = percent(value/100)), size=0)
 
+#The piechart....
 pie
 
 
 
-#To do list for the project--------
 
-#Change the scale on the below graph on the Y axis
+#To do list for the project----------------------------------------------------
 
-data2 <- read_excel("~/myfinalproject/processed/data2.xlsx")
-View(data2)
+#Change the scale on the below graph on the Y axis, adjust the points on the axis
+#Add a title to the graph 
+#Distribute clustered data points using the dodge function - 
 
-d2<- ggplot(data2, aes(x = factor(years), y = number, fill = Country, 
-                       group = Country)) +
+geom_line(position = position_dodge(0.2)) +           # Dodge lines by 0.2
+  geom_point(position = position_dodge(0.2), size = 4)  # Dodge points by 0.2
+
+
+rs2plot <-ggplot(rs2_long, aes(x = Year, y = Number, 
+  color = Region, group = Region)) +
+  geom_point() +
   geom_line() +
-  geom_point(size = 2, shape = 21) +
-  labs(y="Number of People", x = "Years") +
-  labs(title = "Number of Rough Sleepers in UK by region")
+  scale_color_brewer(palette = 'Dark2') +
+  theme_classic(base_size = 10)
 
-d2
+rs2plot
+
 
 #Look at the data wrangling recording at how to manipulate the data
 
@@ -159,3 +177,34 @@ rs1plot <- ggplot(rs1_long, aes(x = Year, y = Number, color = Region, group = Re
   theme_classic(base_size = 12)
 
 rs1plot
+
+#Replace data currently in pie chart with the RS in each region of England data
+
+data4 <- read_excel("~/myfinalproject/processed/data4.xlsx")
+
+d4 <- data.frame(
+  Group = c("Leeds", "Sheffield", "Kingston upon Hull, City of", "Doncaster", 
+            "East Riding of Yorkshire", "Scarborough", "Barnsley"),
+  value = c(35, 24, 19, 13, 11, 11, 10)
+)
+
+bp<- ggplot(d4, aes(x="", y=value, fill=Group))+
+  geom_bar(width = 1, stat = "identity")
+
+pie <- bp + coord_polar("y", start=0)
+
+blank_theme <- theme_minimal()+
+  theme(
+    axis.title.x = element_blank(),
+    axis.title.y = element_blank(),
+    panel.border = element_blank(),
+    panel.grid=element_blank(),
+    plot.title=element_text(size=14, face="bold")
+  )
+
+pie<- pie + scale_fill_brewer(palette="Dark2") + blank_theme +
+  theme(axis.text.x=element_blank())+
+  geom_text(aes(y = value/3 + c(0, cumsum(value)[-length(value)]), 
+                label = percent(value/100)), size=0)
+
+pie
